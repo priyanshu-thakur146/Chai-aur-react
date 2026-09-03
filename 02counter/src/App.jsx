c966aa985351c93ba7950bcbee3def071fcdf852
priyanshu thakur <priyqnshu94@gmail.com>
2026-09-03 06:42:50 +0530

    hooks in react

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -7,12 +7,12 @@ import './App.css'
 function App() {
 let [count, setCount] = useState(0);
 const increment = () => {
-  if(count<=20){
+  if(count<20){
     setCount(count + 1);
   }
 };
 const decrement = () => {
-  if(count>=0){
+  if(count>0){
     setCount(count - 1);
   }
 };
